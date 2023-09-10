42903619ed44bbfbd2163185bb0398fd5a59e5fc
Manu <manugeo13@gmail.com>
2023-09-10 13:18:53 +0530

    Sign Up - Initial UI implementation.

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,15 +1,40 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import theme from "../theme";
+import { useState } from "react";
+import InputText from "./InputText";
+import InputButton from "./InputButton";
 
-const SignUp = ({ navigation }) => {
-  const styles = StyleSheet.create({
-    container: { }
-  });
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+    backgroundColor: theme.colors.backgroundPrimary
+  },
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  }
+});
+
+const SignUp = () => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const onSignUpPress = () => {
+    console.log("Button pressed!");
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Sign Up Page!</Text>
-      <Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollViewContent} >
+      <View style={styles.container}>
+        <InputText value={fullName} label="Full Name" onChange={setFullName} />
+        <InputText value={email} label="Email" keyboardType="email-address" style={{ marginTop: 20 }} onChange={setEmail} />
+        <InputText value={password} label="Password" secureTextEntry style={{ marginTop: 20 }} onChange={setPassword} />
+        <InputText value={confirmPassword} label="Confirm Password" secureTextEntry style={{ marginTop: 20 }} onChange={setConfirmPassword} />
+        <InputButton title="Sign Up" style={{ marginTop: 60 }} onPress={onSignUpPress} />
+      </View>
+    </ScrollView>
   );
 };
 
