8e26478b281587dd7c1bd1bae98c7f133325708c
Manu <manugeo13@gmail.com>
2023-09-10 11:53:05 +0530

    Updated header styles.

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/components/Login';
 import SignUp from './src/components/SignUp';
+import theme from './src/theme';
 
 const Stack = createNativeStackNavigator();
+const { colors } = theme;
 
 export default function App() {
+  const screenOptions = {
+    headerTitleAlign: 'center',
+    headerTintColor: colors.textPrimary,
+    headerStyle: { backgroundColor: colors.backgroundPrimary }
+  };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='SignUp' screenOptions={screenOptions}>
         <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='SignUp' component={SignUp} options={{ title: 'Sign Up'}} />
+        <Stack.Screen name='SignUp' component={SignUp} options={{ title: 'Sign Up' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
